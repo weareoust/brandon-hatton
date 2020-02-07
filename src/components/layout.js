f507af1576fdb3778d3e739f28d8624ab0384bee
matthewapower <matthewallenpower@gmail.comm>
2020-02-07 13:39:57 -0500

    Adding notification

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,59 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
+import { Link } from "gatsby"
 import tw from 'tailwind.macro'
 import { css } from '@emotion/core'
 import TransitionLink from 'gatsby-plugin-transition-link'
-import { TweenLite } from 'gsap/all'
+import { TweenMax, TimelineMax } from 'gsap/all'
 import SEO from "../components/seo"
 import Drawer from "../components/drawer"
 import logo from "../../content/assets/logo.svg"
 import "../components/global.css"
+import { Body } from "./type"
+
+function Alert(props) {
+  const { show } = props
+  const alert = useRef(null);
+  const [timeline, setTimeline] = useState(new TimelineMax());
+
+  useEffect(() => {
+    if (show) {
+      timeline
+      .fromTo(alert.current, 1, {transform: "translateX(100px)", opacity: 0}, {delay: 5, transform: "translateX(0)", opacity: 1})
+      .to(alert.current, 1, {delay: 3, transform: "translateX(100px)", opacity: 0})
+    }
+  }, [show])
+
+  if (show) {
+    return (
+      <Link 
+        to="/impact-map" 
+        css={tw`bg-sidebar-gray text-black shadow-lg md:absolute top-0 right-0 m-8 md:m-24 p-4 rounded opacity-0`}
+        ref={alert}
+      >
+        <Body css={tw`m-0`}>Check out the IMPACTMAP</Body>
+      </Link>
+    )
+  } else {
+    return null
+  }
+}
 
 export default function Layout(props) {
   let expand = props.expand
   const [drawerOffset, setDrawerOffset] = useState(0)
+  const [showAlert, setShowAlert] = useState(typeof window !== 'undefined' && localStorage["visited"] ? false : true)
+
+  useEffect(() => {
+    if (showAlert === true) {
+      localStorage["visited"] = true
+    }
+  }, [showAlert])
 
   const closeDrawer = { 
     trigger: () => {
-      TweenLite.to('#sidebar', 0.3, { transform: drawerOffset })
-      TweenLite.fromTo('#nav', 0.3, { opacity: 0 }, { opacity: 1 })
-      TweenLite.fromTo('#content', 0.3, { opacity: 1 }, { opacity: 0 })
+      TweenMax.to('#sidebar', 0.3, { transform: drawerOffset })
+      TweenMax.fromTo('#nav', 0.3, { opacity: 0 }, { opacity: 1 })
+      TweenMax.fromTo('#content', 0.3, { opacity: 1 }, { opacity: 0 })
     },
     length: 0.3
   }
@@ -24,7 +61,8 @@ export default function Layout(props) {
   const fadeIn = { 
     delay: 0.3,
     trigger: () => {
-      TweenLite.fromTo('#featureContent', 0.6, { opacity: 0, transform: 'translateY(-40px)' }, { opacity: 1, transform: 'none' })
+      TweenMax.fromTo('#featureContent', 0.6, 
+        { opacity: 0, transform: 'translateY(-40px)' }, { opacity: 1, transform: 'none' })
     },
     length: 0.6
   }
@@ -33,7 +71,12 @@ export default function Layout(props) {
   return (
     <main css={tw`relative overflow-hidden`}>
       <TransitionLink to="/" exit={closeDrawer} entry={fadeIn} css={css`
-        ${tw`h-screen w-screen block p-8 md:p-24 relative`}
+        ${tw`block p-8 md:p-24 relative w-screen h-screen`}
+
+        @media (min-width: 768px) {
+          height: 100vh;
+          width: calc(100vw - 400px);
+        }
 
         &::before {
           content: '';
@@ -44,6 +87,7 @@ export default function Layout(props) {
         }
       `}>
         <SEO title="Home" />
+        <Alert show={showAlert} reset={setShowAlert} />
         <div id="featureContent" css={css`
           display: ${expand ? 'none' : 'block'};
         `}>

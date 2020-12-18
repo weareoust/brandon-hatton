419efe24c4378b0d799643b4d16d30f4e39b347f
matthewapower <matthewallenpower@gmail.comm>
2020-12-18 09:39:55 -0500

    Update seo component.

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title, image }) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate={`%s | ${title || site.siteMetadata.title}`}
       meta={[
         {
           name: `description`,

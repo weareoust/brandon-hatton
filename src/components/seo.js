ba5a32f1da6795dc4b8a22a20ae1cc49600345ec
matthewapower <matthewallenpower@gmail.comm>
2020-12-22 12:52:49 -0500

    Update Title on pages.

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title, image }) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${title || site.siteMetadata.title}`}
+      titleTemplate={title || site.siteMetadata.title}
       meta={[
         {
           name: `description`,

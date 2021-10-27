d88442719d3569556ad33959244575abc8f7e30b
Matt Power <matthewallenpower@gmail.com>
2021-10-27 10:01:18 -0400

    Update twitter images

--- src/components/seo.js
+++ src/components/seo.js
@@ -30,7 +30,7 @@ function SEO({ description, lang, meta, title, image }) {
   let formattedImage = image 
 
   if (image && image.includes('https:') == false) {
-    formattedImage = image.replace(/\/\//, 'http://')
+    formattedImage = image.replace(/\/\//, 'https://')
   } 
 
   return (
@@ -59,6 +59,10 @@ function SEO({ description, lang, meta, title, image }) {
         },
         {
           property: `og:image`,
+          content: image ? formattedImage : "",
+        },
+        {
+          property: `twitter:image`,
           content: image ? formattedImage : "",
         },
         {
